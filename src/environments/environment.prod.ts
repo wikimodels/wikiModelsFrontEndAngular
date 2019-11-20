2ff9ff830e62850b4ce55339655d8c057c8be352
wikimodelsstore <vitali.wiki.models@gmail.com>
2019-11-20 21:25:34 +0300

    added error handler

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  // tslint:disable-next-line:max-line-length
-  apiUrl: 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/wikimodelssitestitch-izpxe/service/WikiModelsSiteService/incoming_webhook/'
-};
